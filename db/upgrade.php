f63272279fad053913b7ad0cbfe914e126f377a0
Jesús PL <jesusgorillo@gmail.com>
2020-01-31 18:11:49 +0100

    Discard changes in this file

--- db/upgrade.php
+++ db/upgrade.php
@@ -308,7 +308,7 @@ function xmldb_plagiarism_plagscan_upgrade($oldversion) {
             $dbman->change_field_default($table, $field);
         }
 
-        $field = new xmldb_field('submissionid', XMLDB_TYPE_CHAR, '255', XMLDB_UNSIGNED, null, null, 'ownerid');
+        $field = new xmldb_field('submissionid', XMLDB_TYPE_CHAR, '255', XMLDB_UNSIGNED, null, null, null);
         if ($dbman->field_exists($table, $field)) {
             $dbman->change_field_default($table, $field);
         }
